cd1885e9780c2d3dc1dc3a431fef511080eb1f30
Amritha <amrithagk12@gmail.com>
2023-06-27 22:38:50 +0530

    automated upto add to cart

--- Automate Add to cart/steps/addtocart.py
+++ Automate Add to cart/steps/addtocart.py
@@ -1,11 +1,12 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 @given(u'User launches Google Chrome web browser')
 def step_impl(context):
     context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(40)
+    context.driver.implicitly_wait(90)
 
 
 @given('User opens www.amazon.in')
@@ -43,14 +44,15 @@ def step_impl(context):
     search_box.send_keys("SONY 55inch TV")
     context.driver.find_element(By.ID, 'nav-search-submit-button').click() #click on the search icon
 
-"""
+
 @then(u'Clicks on the desired item')
 def step_impl(context):
-    assert True
-
-"""
+    context.driver.execute_script("""window.open('https://www.amazon.in/Sony-Bravia-inches-Google-KD-55X74K/dp/B09WN26DG5/ref=sr_1_3?crid=Q4MO3BD2PSZG&keywords=SONY+55inch+TV&qid=1687795413&sprefix=sony+55inch+tv%2Caps%2C500&sr=8-3', '_blank');""")
+    context.driver.switch_to.window(window_name=context.driver.window_handles[-1])
 
 
 @then(u'Adds the item to the shopping cart')
 def step_impl(context):
-    assert True
+    context.driver.find_element(By.ID, "add-to-cart-button").click()
+    time.sleep(10)
+    context.driver.close()
