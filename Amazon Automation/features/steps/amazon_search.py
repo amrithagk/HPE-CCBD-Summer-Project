28b431ec4842f46e8c00a93514c77cef82781d87
liajaned <117766962+liajaned@users.noreply.github.com>
2023-06-28 12:17:55 +0530

    Update amazon_search.py

--- Amazon Automation/features/steps/amazon_search.py
+++ Amazon Automation/features/steps/amazon_search.py
@@ -27,8 +27,6 @@ def step_impl(context):
 
 @when('find the third product')
 def step_impl(context):
-    #text = context.driver.find_element("xpath","//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]").text
-    #assert text == "Sony Bravia 139 cm (55 inches) 4K Ultra HD Smart LED Google TV KD-55X74K (Black)"
     window_before=context.driver.window_handles[0]
     context.driver.find_element("xpath","//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]").click()
     time.sleep(10)
@@ -38,7 +36,6 @@ def step_impl(context):
 
 @when('click add to cart button')
 def step_impl(context):
-    #context.driver.get("https://www.amazon.in/Sony-Bravia-inches-Google-KD-55X74K/dp/B09WN26DG5/ref=sr_1_3?crid=NFEQG485MSTR&keywords=sony%2B55%2Binch%2Btv&qid=1687885969&sprefix=sony%2B55%2B%2Caps%2C775&sr=8-3&th=1")
     context.driver.find_element(By.ID, "add-to-cart-button").click()
     time.sleep(10)
 
