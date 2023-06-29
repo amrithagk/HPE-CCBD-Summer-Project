4fa21c77a78dfed914e52f6e01ea67742a471a94
liajaned <117766962+liajaned@users.noreply.github.com>
2023-06-29 21:05:10 +0530

    Update product_search.py

--- Automate add to cart/steps/product_search.py
+++ Automate add to cart/steps/product_search.py
@@ -19,7 +19,7 @@ def step_impl(context):
     context.driver.find_element(By.ID,"nav-search-submit-button").click()
 
 
-@when('find the third product')
+@then('find the third product')
 def step_impl(context):
     window_before=context.driver.window_handles[0]
     context.driver.find_element("xpath","//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]").click()
