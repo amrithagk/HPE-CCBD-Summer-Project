b8e2bc6cf96d2535a523ea671d5c530e4c378872
Amritha <amrithagk12@gmail.com>
2023-06-29 08:12:01 +0530

    added exception handling

--- Automate Add to cart/steps/displayoffers.py
+++ Automate Add to cart/steps/displayoffers.py
@@ -1,18 +1,25 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException
 import time
 
 
 @then(u'Displays the offers')
 def step_impl(context):
 
-    context.driver.find_element(By.ID, "attach-close_sideSheet-link").click()
-    context.driver.implicitly_wait(10)
+    try:
+        context.driver.find_element(By.ID, "attach-close_sideSheet-link").click()
+        context.driver.implicitly_wait(10)
 
-    #returns all 3 cards in carousel, hence access the bank offers by indexing the array
-    offers = context.driver.find_element(By.XPATH, "//a[@class='a-size-base a-link-emphasis vsx-offers-count']")
-    context.driver.execute_script("arguments[0].click();", offers)
-    
-    time.sleep(10)
-    context.driver.close()
+        #returns all 3 cards in carousel, hence access the bank offers by indexing the array
+        offers = context.driver.find_element(By.XPATH, "//a[@class='a-size-base a-link-emphasis vsx-offers-count']")
+        context.driver.execute_script("arguments[0].click();", offers)
+        
+        time.sleep(10)
+        context.driver.close()
+
+    except NoSuchAttributeException or NoSuchElementException:
+
+        print("Exception occurred")
+        context.driver.close()
