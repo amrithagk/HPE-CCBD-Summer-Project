79cb1e5c5ac09b027c3994d1589dbb0abcb25c03
liajaned <117766962+liajaned@users.noreply.github.com>
2023-06-29 20:37:48 +0530

    Update add_to_cart.py

--- Automate add to cart/steps/add_to_cart.py
+++ Automate add to cart/steps/add_to_cart.py
@@ -1,3 +1,14 @@
+import time
+from telnetlib import EC
+
+from behave import *
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 @when('click add to cart button')
 def step_impl(context):
     context.driver.find_element(By.ID, "add-to-cart-button").click()
