1d0c1af25022ffcfece35886e9d989b4e8fa1ae2
liajaned <117766962+liajaned@users.noreply.github.com>
2023-06-29 10:57:13 +0530

    Update amazon_search.py

--- Amazon Automation/features/steps/amazon_search.py
+++ Amazon Automation/features/steps/amazon_search.py
@@ -1,13 +1,18 @@
 import time
+from telnetlib import EC
 
 from behave import *
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @given(u'launch chrome browser')
 def step_impl(context):
     context.driver=webdriver.Chrome()
+    context.driver.maximize_window()
 
 
 @when('open amazon home page')
@@ -29,9 +34,32 @@ def step_impl(context):
 def step_impl(context):
     window_before=context.driver.window_handles[0]
     context.driver.find_element("xpath","//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]").click()
-    time.sleep(10)
+    time.sleep(5)
     window_after=context.driver.window_handles[1]
     context.driver.switch_to.window(window_after)
+
+
+@when('fetch price of the product')
+def step_impl(context):
+    price = context.driver.find_element("xpath", " //body/div[@id='a-page']/div[@id='dp']/div[@id='dp-container']/div[@id='ppd']/div[@id='centerCol']/div[@id='apex_desktop']/div[@id='corePriceDisplay_desktop_feature_div']/div[1]/span[2]/span[2]/span[2]")
+    context.driver.execute_script("arguments[0].scrollIntoView();", price)
+    time.sleep(10)
+    product_price=price.text
+    print(product_price)
+
+
+@when('fetch the description of product')
+def step_impl(context):
+    element=context.driver.find_element("xpath"," //h1[contains(text(),'About this item')]")
+    context.driver.execute_script("arguments[0].scrollIntoView();",element)
+    time.sleep(10)
+    product_description=[]
+    description_element = context.driver.find_elements(By.CLASS_NAME, 'a-unordered-list a-vertical a-spacing-mini')
+    for description in description_element:
+        product_description.append(description.text)
+    for text in product_description:
+        print(text)
+
 
 
 @when('click add to cart button')
