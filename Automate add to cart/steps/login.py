cba61f17e4c46b5717d9e848a71c11bc3c3b5ab3
Sinchana13 <107702267+Sinchana13@users.noreply.github.com>
2023-06-29 20:27:32 +0530

    Create login.py

---
+++ Automate add to cart/steps/login.py
@@ -0,0 +1,44 @@
+from behave import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@given('I launch Chrome browser')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+
+
+@when('I visit amazon website')
+def step_impl(context):
+    context.driver.get("https://www.amazon.in/")
+
+
+@then('Amazon homepage is opened successfully')
+def step_impl(context):
+    text = context.driver.title
+    assert text == "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in"
+
+
+@given('I am on the Amazon e-commerce site')
+def step_impl(context):
+    pass
+
+
+@when('I log in with valid phone number or email "{user}" and password "{pwd}"')
+def step_impl(context, user, pwd):
+    signin = context.driver.find_element(By.ID, "nav-link-accountList")
+    signin.click()
+    signin_input = context.driver.find_element(By.ID, "ap_email")
+    signin_input.send_keys(user)
+    context.driver.find_element(By.ID, "continue").click()
+    pwd_input = context.driver.find_element(By.ID, "ap_password")
+    pwd_input.send_keys(pwd)
+    context.driver.find_element(By.ID, "signInSubmit").click()
+
+@then('I should login successfully')
+def step_impl(context):
+    name = context.driver.find_element(By.ID, "nav-link-accountList-nav-line-1").text
+    assert name == "Hello, SUMA"
+    
+
+
