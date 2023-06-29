0bed441a362ecb3ba3c04ffcb1f9a078c85c78a3
Nisarga Kunder <114926827+nisargakunder@users.noreply.github.com>
2023-06-29 17:47:06 +0530

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -6,28 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@given('the user is on the Amazon homepage')
-def step_user_on_homepage(context):
-    context.driver = webdriver.Chrome()  # Use the appropriate WebDriver for your browser
-    context.driver.get('https://www.amazon.com')
-
-@when('the user searches for "{search_query}"')
-def step_user_searches(context, search_query):
-    search_box = context.driver.find_element(By.ID, 'twotabsearchtextbox')
-    search_box.clear()  # Clear the search box
-    search_box.send_keys(search_query)
-    search_box.send_keys(Keys.RETURN)
-
-@when('the user clicks on the 3rd search result')
-def step_user_clicks_third_result(context):
-    search_results = context.driver.find_elements(By.CSS_SELECTOR, '.s-result-list .s-result-item')
-    if len(search_results) > 3:
-        third_result = search_results[3]
-        third_result_link = third_result.find_element(By.CSS_SELECTOR, 'a')
-        third_result_link.click()
-    else:
-        context.driver.quit()
-
 
 @when('the user navigates to the reviews section')
 def navigates_to_reviews(context):
