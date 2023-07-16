6e8b55cbf497bc2ff209d32c43c28dd2d2aa685b
Amritha GK <101631715+amrithagk@users.noreply.github.com>
2023-07-16 19:51:57 +0530

    Update displayoffers.py

--- Automate add to cart/steps/displayoffers.py
+++ Automate add to cart/steps/displayoffers.py
@@ -3,11 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException
 import time
-
+import logging
 
 @then(u'Displays the offers')
 def step_impl(context):
-
     try:
         context.driver.find_element(By.ID, "attach-close_sideSheet-link").click()
         context.driver.implicitly_wait(10)
@@ -17,8 +16,9 @@ def step_impl(context):
         context.driver.execute_script("arguments[0].click();", offers)
         time.sleep(5)
         context.driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-close-white a-icon-medium twister-plus-close-button']").click()
+
+        logging.info("Displayed offers")
         
     except NoSuchAttributeException or NoSuchElementException:
-
-        print("Exception occurred")
+        logging.info("Offers not found")
         context.driver.close()
