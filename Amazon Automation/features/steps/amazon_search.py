f6df57ce1aa6d83b6671b67ec3920feb2f5985db
liajaned <117766962+liajaned@users.noreply.github.com>
2023-07-16 19:13:14 +0530

    Update amazon_search.py

--- Amazon Automation/features/steps/amazon_search.py
+++ Amazon Automation/features/steps/amazon_search.py
@@ -72,6 +72,23 @@ def step_impl(context):
 def step_impl(context):
     context.driver.close()
 
+@then('send email notification')
+def step_impl(context):
+    print("Task completed")
+    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        sender="hellojaned123@gmail.com"
+        sender_password="rzlzacazmtdkoqoj"
+        receiver="hellojaned123@gmail.com"
+        smtp.login(sender,sender_password)
+        subject="The program was executed successfully"
+        body="Congratulations!Your program was successful"
+        message=f'Subject: {subject}\n\n{body}'
+        smtp.sendmail(sender,receiver,message)
+        print("Email sent!")
+
 
 
 
