653436e9a80f22f2485793bd7bd1b537d3140011
Amritha GK <101631715+amrithagk@users.noreply.github.com>
2023-07-16 21:00:49 +0530

    added logging

--- Automate add to cart/steps/reviews.py
+++ Automate add to cart/steps/reviews.py
@@ -5,10 +5,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import logging
 
 @then('the user navigates to the reviews section')
 def navigates_to_reviews(context):
     context.driver.switch_to.window(context.driver.window_handles[-1])
+    logging.info("Navigated to review section")
     try:
         read_all_reviews_link = WebDriverWait(context.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//a[@data-hook="see-all-reviews-link-foot"]'))
@@ -16,6 +18,7 @@ def navigates_to_reviews(context):
         read_all_reviews_link.click()
         time.sleep(10)
     except:
+        logging.info("Couldn't locate reviews")
         context.driver.quit()
 
 @then('the user should be able to view the reviews')
@@ -25,9 +28,11 @@ def views_reviews(context):
             EC.presence_of_element_located((By.CSS_SELECTOR, '.a-section.review.aok-relative'))
         )
         reviews = context.driver.find_elements(By.CSS_SELECTOR, '.a-section.review.aok-relative')
+        logging.info("Opened reviews page")
         time.sleep(10)
         for review in reviews:
             print(review.text)
-
+    except:
+        logging.info("Couldn't open reviews page")
     finally:
         context.driver.quit()
