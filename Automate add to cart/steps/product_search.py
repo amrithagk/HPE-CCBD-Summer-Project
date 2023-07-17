e0a1d56ee14882b0e5c05ecb7653b71918109aeb
Amritha GK <101631715+amrithagk@users.noreply.github.com>
2023-07-17 13:50:46 +0530

    Update product_search.py

--- Automate add to cart/steps/product_search.py
+++ Automate add to cart/steps/product_search.py
@@ -33,11 +33,15 @@ def step_impl(context):
     try:
         context.driver.find_element(By.ID,"nav-search-submit-button").click()
         logging.info("Clicked on search button")
-    except:
+        time.sleep(3)
         assert "No results for" in context.driver.page_source
-        time.sleep(10)
         screenshot_path3 = os.path.join(screenshot_dir, "unsuccessful_search.png")
         context.driver.save_screenshot(screenshot_path3)
+        logging.info("Product not found")
+        allure.attach("Product not found", attachment_type=AttachmentType.TEXT)
+        allure.attach(context.driver.get_screenshot_as_png(), name="Unsuccessful search", attachment_type=AttachmentType.PNG)
+    except AssertionError:
+        logging.error("Error occurred")
     if not os.path.exists(screenshot_dir):
         os.makedirs(screenshot_dir)
 
@@ -50,12 +54,13 @@ def step_impl(context):
             context.driver.find_element("xpath","//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/h2[1]/a[1]/span[1]").click()
         except NoSuchElementException:
             logging.info("Couldn't locate the product")
-            allure.attach("Couldn't locate the product", attachment_type=AttachmentType.TEXT)
+            allure.attach("Product not found", attachment_type=AttachmentType.TEXT)
         time.sleep(5)
         window_after=context.driver.window_handles[1]
         context.driver.switch_to.window(window_after)
         logging.info("Opened the product on a new tab")
-    except Exception as e:
+    except IndexError:
         logging.error("Couldn't open new tab")
         allure.attach("Couldn't open new tab", attachment_type=AttachmentType.TEXT)
-        raise e
+        context.driver.close()
+        raise Exception("Product not found")
