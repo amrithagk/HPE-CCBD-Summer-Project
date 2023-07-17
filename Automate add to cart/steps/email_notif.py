d9b4e7ab180461c6ac5e20f84046e0f13d96c8a9
liajaned <117766962+liajaned@users.noreply.github.com>
2023-07-17 16:17:13 +0530

    Update email_notif.py

--- Automate add to cart/steps/email_notif.py
+++ Automate add to cart/steps/email_notif.py
@@ -40,16 +40,4 @@ Link for the allure report: <a href="https://allure-reports-summerproject.netlif
 
 
     
-    with smtplib.SMTP('smtp.gmail.com',587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-        sender="hellojaned123@gmail.com"
-        sender_password="rzlzacazmtdkoqoj"
-        receiver="hellojaned123@gmail.com"
-        smtp.login(sender,sender_password)
-        subject="The program was executed successfully"
-        body="Task successful"
-        message=f'Subject: {subject}\n\n{body}'
-        smtp.sendmail(sender,receiver,message)
-        print("Email sent!")
+   
