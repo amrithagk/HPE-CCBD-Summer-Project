098040751ead212ebf0806ae619e5d5567da0cc9
Amritha <101631715+amrithagk@users.noreply.github.com>
2023-08-03 01:25:46 +0530

    added logging

--- Automate add to cart/steps/email_notif.py
+++ Automate add to cart/steps/email_notif.py
@@ -5,37 +5,37 @@ from email.mime.text import MIMEText
 
 from behave import *
 from selenium import webdriver
+import logging
 
+logging.basicConfig(filename="amazonlog.log", format="%(asctime)s  %(levelname)s:%(message)s", level=logging.INFO)
 
 @then('send email notification')
 def step_impl(context):
-    print("Task completed")
-    sender = "hellojaned123@gmail.com"
-    sender_password = "dariqfjgajewxtev"
-    receiver = "hellojaned123@gmail.com"
-    msg = MIMEMultipart()
-
-    msg['From'] = sender
-    msg['To'] = receiver
-    msg['Subject'] = "The program was executed successfully"
-    body = """<pre> 
-Task successful
-Link for the allure report: <a href="https://allure-reports-summerproject.netlify.app/">click here</a>
-</pre>"""
-    msg.attach(MIMEText(body, 'html'))
-
-    
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.starttls()
+    try:
+        print("Task completed")
         sender = "hellojaned123@gmail.com"
         sender_password = "dariqfjgajewxtev"
         receiver = "hellojaned123@gmail.com"
-        smtp.login(sender, sender_password)
-        text = msg.as_string()
-        smtp.sendmail(sender,receiver,text)
+        msg = MIMEMultipart()
 
+        msg['From'] = sender
+        msg['To'] = receiver
+        msg['Subject'] = "The program was executed successfully"
+        body = """<pre> 
+    Task successful
+    Link for the allure report: <a href="https://allure-reports-summerproject.netlify.app/">click here</a>
+    </pre>"""
+        msg.attach(MIMEText(body, 'html'))
 
-
-
-    
-   
+        
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.starttls()
+            sender = "hellojaned123@gmail.com"
+            sender_password = "dariqfjgajewxtev"
+            receiver = "hellojaned123@gmail.com"
+            smtp.login(sender, sender_password)
+            text = msg.as_string()
+            smtp.sendmail(sender,receiver,text)
+            logging.info("E-mail sent!")
+    except:
+        logging.error("Error occurred. Could not send e-mail")
